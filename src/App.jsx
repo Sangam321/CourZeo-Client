6f7389263b89cb1a2b588b4a289f17058606029f
Sangam Basnet <sangambasnet1800@gmail.com>
2025-07-12 12:27:54 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,163 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import {
+  AdminRoute,
+  AuthenticatedUser,
+  ProtectedRoute,
+} from "./components/ProtectedRoutes";
+import PurchaseCourseProtectedRoute from "./components/PurchaseCourseProtectedRoute";
+import { ThemeProvider } from "./components/ThemeProvider";
+import MainLayout from "./layout/MainLayout";
+
+import Login from "./pages/Login";
+import Dashboard from "./pages/admin/Dashboard";
+import Sidebar from "./pages/admin/Sidebar";
+import AddCourse from "./pages/admin/course/AddCourse";
+import CourseTable from "./pages/admin/course/CourseTable";
+import EditCourse from "./pages/admin/course/EditCourse";
+import CreateLecture from "./pages/admin/lecture/CreateLecture";
+import EditLecture from "./pages/admin/lecture/EditLecture";
+
+import AboutUs from "./pages/student/About_us";
+import CourseDetail from "./pages/student/CourseDetail";
+import CourseProgress from "./pages/student/CourseProgress";
+import Courses from "./pages/student/Courses";
+import HeroSection from "./pages/student/HeroSection";
+import MyLearning from "./pages/student/MyLearning";
+import Profile from "./pages/student/Profile";
+import SearchPage from "./pages/student/SearchPage";
+import Services from "./pages/student/Services";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <>
+            <HeroSection />
+            <Courses />
+            <Services />
+            <AboutUs />
+          </>
+        ),
+      },
+      {
+        path: "courses",
+        element: (
+
+          <Courses />
+
+        ),
+      },
+      {
+        path: "services",
+        element: <Services />,
+      },
+      {
+        path: "about",
+        element: <AboutUs />,
+      },
+      {
+        path: "login",
+        element: (
+          <AuthenticatedUser>
+            <Login />
+          </AuthenticatedUser>
+        ),
+      },
+      {
+        path: "my-learning",
+        element: (
+          <ProtectedRoute>
+            <MyLearning />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "profile",
+        element: (
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "course/search",
+        element: (
+          <ProtectedRoute>
+            <SearchPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "course-detail/:courseId",
+        element: (
+          <ProtectedRoute>
+            <CourseDetail />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "course-progress/:courseId",
+        element: (
+          <ProtectedRoute>
+            <PurchaseCourseProtectedRoute>
+              <CourseProgress />
+            </PurchaseCourseProtectedRoute>
+          </ProtectedRoute>
+        ),
+      },
+
+      // admin routes
+      {
+        path: "admin",
+        element: (
+          <AdminRoute>
+            <Sidebar />
+          </AdminRoute>
+        ),
+        children: [
+          {
+            path: "dashboard",
+            element: <Dashboard />,
+          },
+          {
+            path: "course",
+            element: <CourseTable />,
+          },
+          {
+            path: "course/create",
+            element: <AddCourse />,
+          },
+          {
+            path: "course/:courseId",
+            element: <EditCourse />,
+          },
+          {
+            path: "course/:courseId/lecture",
+            element: <CreateLecture />,
+          },
+          {
+            path: "course/:courseId/lecture/:lectureId",
+            element: <EditLecture />,
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <main>
+      <ThemeProvider>
+        <RouterProvider router={appRouter} />
+      </ThemeProvider>
+    </main>
+  );
+}
+
+export default App;
